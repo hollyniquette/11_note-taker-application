b2ec3926202052876fa04d77245b0ff9f2cb9ee1
hollyniquette <127768285+hollyniquette@users.noreply.github.com>
2023-08-20 15:45:37 -0500

    completed & updated readme

---
+++ README.md
@@ -0,0 +1,32 @@
+# E-Commerce Back End • ORM
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [License](#license)
+- [Questions](#questions)
+
+## Description
+
+The Note Taker application allows a user to write and save notes using an Express.js back end, retrieving note data from a JSON file. The user can organize their thoughts and delete tasks as they complete them.
+
+## Installation and Usage
+
+- Install required dependencies with `npm i`
+- Use `npm start` to start the server
+- Navigate to `http://localhost:3001` in your browser to view the application
+
+## Guidelines for Contribution
+
+All suggestions for improvement welcome! Feel free to fork the project and submit a pull request with your changes.
+
+## License
+
+This project is covered under the MIT license.
+
+## Questions
+
+Reach out to me via [Github](https://github.com/hollyniquette) or [Email me](mailto:hollyniquette@gmail.com) with any questions or issues you might have.
---
+++ server.js
@@ -0,0 +1,69 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { nanoid as nanoId } from 'nanoid';
+import fs from 'fs';
+
+const app = express();
+
+/* target public folder for client files */
+app.use(express.static('public'));
+app.use(express.json());
+
+const PORT = 3001;
+
+/* __dirname fix for ES6 modules */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/* sends index.html to client */
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+/* sends notes.html to client */
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+/* instantiate listener */
+app.listen(PORT, () =>
+  console.log(`Note Taker server listening at http://localhost:${PORT}`)
+);
+
+/* load notes from db.json */
+app.get('/api/notes', (req, res) => {
+  const notes = JSON.parse(
+    fs.readFileSync(path.join(__dirname, './db/db.json'))
+  );
+  res.status(200).json(notes);
+});
+
+/* add new note to db.json */
+app.post('/api/notes', (req, res) => {
+  let note = req.body;
+  const existingNotes = JSON.parse(
+    fs.readFileSync(path.join(__dirname, './db/db.json'))
+  );
+  note.id = nanoId();
+  existingNotes.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify(existingNotes)
+  );
+  res.status(200).json('Note added!');
+});
+
+/* delete note from db.json */
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const existingNotes = JSON.parse(
+    fs.readFileSync(path.join(__dirname, './db/db.json'))
+  );
+  const filteredNotes = existingNotes.filter((note) => note.id !== id);
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify(filteredNotes)
+  );
+  res.status(200).json('Note deleted!');
+});
